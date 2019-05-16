4863f6840ac671156d9bbfbb217b0412fb9fcecc
yoee <yoee.kim@gmail.com>
2019-05-16 14:16:12 +0900

    rnn

---
+++ hyu_class/lab/07_rnn/solution.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import tensorflow as tf
+import numpy as np
+import pprint
+
+sentence = ("if you want to build a ship, don't drum up people together to "
+            "collect wood and don't assign them tasks and work, but rather "
+            "teach them to long for the endless immensity of the sea.")
+char_set = list(set(sentence))
+char_dic = {w: i for i, w in enumerate(char_set)}
+
+# In[2]:
+
+
+# hyper parameters
+dic_size = len(char_dic)
+hidden_size = len(char_dic)
+num_classes = len(char_dic)
+batch_size = 1
+sequence_length = len(sentence) - 1
+learning_rate = 0.1
+
+# In[3]:
+
+
+sample_idx = [char_dic[c] for c in sentence]
+x_data = [sample_idx[:-1]]  # 0 ~ n-1
+y_data = [sample_idx[1:]]  # 1 ~ n
+
+X = tf.placeholder(tf.int32, [None, sequence_length])
+Y = tf.placeholder(tf.int32, [None, sequence_length])
+
+x_one_hot = tf.one_hot(X, num_classes)
+
+# In[4]:
+
+
+# RNN architecture
+cell = tf.contrib.rnn.BasicLSTMCell(num_units=hidden_size, state_is_tuple=True)
+initial_state = cell.zero_state(batch_size, tf.float32)
+outputs, _states = tf.nn.dynamic_rnn(cell, x_one_hot, dtype=tf.float32, initial_state=initial_state)
+
+# FC layer
+X_for_fc = tf.reshape(outputs, [-1, hidden_size])
+outputs = tf.contrib.layers.fully_connected(X_for_fc, num_classes, activation_fn=None)
+
+# In[5]:
+
+
+# reshape out for sequence_loss
+outputs = tf.reshape(outputs, [batch_size, sequence_length, num_classes])
+
+weights = tf.ones([batch_size, sequence_length])
+sequence_loss = tf.contrib.seq2seq.sequence_loss(logits=outputs, targets=Y, weights=weights)
+loss = tf.reduce_mean(sequence_loss)
+train = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(loss)
+
+prediction = tf.argmax(outputs, axis=2)
+
+# In[7]:
+
+
+# Training
+with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
+    for i in range(50):
+        l, _ = sess.run([loss, train], feed_dict={X: x_data, Y: y_data})
+        result = sess.run(prediction, feed_dict={X: x_data})
+
+        result_str = [char_set[c] for c in np.squeeze(result)]
+        print(i, "loss:", l)
+
+    # Print
+    results = sess.run(outputs, feed_dict={X: x_data})
+    for j, result in enumerate(results):
+        index = np.argmax(result, axis=1)
+        if j is 0:
+            print(''.join([char_set[t] for t in index]), end='')
+        else:
+            print(char_set[index[-1]], end='')
+
+
+
